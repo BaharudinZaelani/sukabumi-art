f2e436e45155d73297e318ffb8ba98d16ea6c71d
Zaelani <bahar32harz@gmail.com>
2022-07-07 14:24:26 +0700

    responsive for mobile version

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,3 +16,15 @@ notif = {
         });
     }
 }
+
+// get navbar
+const navbar = document.getElementsByClassName("navbar")[0];
+navbar.classList.remove("navbar-transparent");
+
+// if navbar clicked show sidebar
+const toggle = document.getElementsByClassName("navbar-toggle")[0];
+const sidebar = document.getElementsByClassName("sidebar")[0];
+toggle.addEventListener("click", () => {
+    sidebar.classList.add("shadow-lg");
+    sidebar.classList.toggle("show");
+});
